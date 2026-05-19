fbc0b8ccf850065d688de5644d4cde895965b3ef
sairam600582 <sairam143p@gmail.com>
2026-05-19 11:26:18 +0000

    add

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -57,9 +57,16 @@ resource "aws_security_group" "sg-1" {
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
-    description = "HTTP from VPC"
+    description = "HTTP from Jenkins"
     from_port   = 8080
     to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "Port for application"
+    from_port   = 5000
+    to_port     = 5000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
